8c32875eae9d2ace972eb1387306b0af50ddcc1b
Md. Ibrahim Siddiquee <ibrahim.siddiqueee@gmail.com>
2023-07-31 00:58:51 +0600

    added routers

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import ErrorPage from "./components/ErrorPage";
+import Home from "./components/Home";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
